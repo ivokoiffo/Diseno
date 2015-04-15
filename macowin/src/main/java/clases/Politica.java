4d9c8abfafeedf03ca86f5f412a4730f67937e89
briansng <singereisky@gmail.com>
2015-04-14 21:00:11 -0300

    Comento en la clase 'Politica' sobre el funcionamiento de la misma.

--- macowin/src/main/java/clases/Politica.java
+++ macowin/src/main/java/clases/Politica.java
@@ -4,6 +4,14 @@ import java.util.*;
 
 public class Politica{
 
+	/*
+	 * SOBRE EL FUNCIONAMIENTO DE ESTA CLASE
+	 * Dentro del sistema, las politicas de aumentos por marca estan generalizadas como bandas de aumentos,
+	 * en relación al precio del producto. Es decir, de este valor a este valor este aumento. 
+	 * Los valores limites de cada tramo estan modelados con la lista 'puntosDeCambio' y los aumentos 
+	 * con la lista 'aumentos'.
+	 * Convenientemente, a las dos listas se les carga en orden sincronizado.
+	 */
 	String nombre;
 	List<Integer> puntosDeCambio;
 	List<Float> aumentos;
@@ -19,14 +27,14 @@ public class Politica{
 
 	public Float aumento(int valor)
 	{
-		if(puntosDeCambio == null) return aumentos.get(0);
+		if(puntosDeCambio == null) return aumentos.get(0); // Si no hay puntos de cambio, entonces es el mismo aumento para todos.
 		for (int i = 0; i < puntosDeCambio.size(); i++ ) 
 		{
 			int min = puntosDeCambio.get(i);
 			int max = puntosDeCambio.get(i+1);
 			if(valor > min && valor < max)
 			{ 
-				return aumentos.get(i);
+				return aumentos.get(i); //Aprovecho que estan sincronizadas.
 			}
 		}
 		return null;
